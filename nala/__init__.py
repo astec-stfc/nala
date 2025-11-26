d7b3d19da0d05da92443f0b4f297df2dd9975ce2
Alex Brynes <alexander.brynes@stfc.ac.uk>
2025-11-26 11:37:18 +0000

    try to let pypi see tags

--- nala/__init__.py
+++ nala/__init__.py
@@ -1,9 +1,6 @@
-from . import _version
 from .nala import NALA
 from . import models
 from . import translator
 from . import Exporters
-
-__version__ = _version.get_versions()["version"]
 
 __all__ = ["NALA", "models"]
